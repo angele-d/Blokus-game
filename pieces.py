2f62e91f5d94e461d35c447a2a24f150b4d2cd47
Thibault <thibault.briand@telecomnancy.eu>
2024-11-09 18:06:42 +0100

    Piece du jeu de blokus en python, via des matrice de BOOL 5*5

---
+++ pieces.py
@@ -0,0 +1,135 @@
+P1 = [[False,False,False,False,False],
+      [False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P2 = [[False,False,False,False,False],
+      [False,False,False,False,False],
+      [False,False,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P3 = [[False,False,False,False,False],
+      [False,False,False,False,False],
+      [False,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P4 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,False,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P5 = [[False,False,False,False,False],
+      [False,False,False,False,False],
+      [True,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P6 = [[False,False,False,False,False],
+      [False,True,False,False,False],
+      [False,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P7 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P8 = [[False,False,False,False,False],
+      [False,False,True,True,False],
+      [False,False,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P9 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,True,True,False,False],
+      [False,True,False,False,False],
+      [False,False,False,False,False]]
+
+P10 = [[False,False,False,False,False],
+      [False,False,False,False,False],
+      [True,True,True,True,True],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P11 = [[False,False,False,False,False],
+      [True,False,False,False,False],
+      [True,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P12 = [[False,False,False,False,False],
+      [False,False,True,True,False],
+      [True,True,True,False,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P13 = [[False,False,False,False,False],
+      [False,True,True,False,False],
+      [False,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P14 = [[False,False,False,False,False],
+      [False,True,False,True,False],
+      [False,True,True,True,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P15 = [[False,False,False,False,False],
+      [False,False,False,True,False],
+      [False,True,True,True,False],
+      [False,True,False,False,False],
+      [False,False,False,False,False]]
+
+
+P17 = [[False,False,False,False,False],
+      [False,False,True,True,False],
+      [False,True,True,False,False],
+      [False,True,False,False,False],
+      [False,False,False,False,False]]
+
+P17 = [[False,False,True,False,False],
+      [False,False,True,False,False],
+      [True,True,True,False,False],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+P18 = [[False,False,False,False,False],
+      [False,True,False,False,False],
+      [False,True,True,True,False],
+      [False,True,False,False,False],
+      [False,False,False,False,False]]
+
+P18 = [[False,False,False,False,False],
+      [False,True,False,False,False],
+      [False,True,True,True,False],
+      [False,True,False,False,False],
+      [False,False,False,False,False]]
+
+P19 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,False,True,True,False],
+      [False,True,True,False,False],
+      [False,False,False,False,False]]
+
+P20 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,True,True,True,False],
+      [False,False,True,False,False],
+      [False,False,False,False,False]]
+
+P21 = [[False,False,False,False,False],
+      [False,False,True,False,False],
+      [False,True,True,True,True],
+      [False,False,False,False,False],
+      [False,False,False,False,False]]
+
+
+
