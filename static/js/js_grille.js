3dc1c1474e1a202384ae14dc66088dbf7260aafa
GIRRES Elisa <elisa.girres@telecomnancy.eu>
2025-01-06 10:26:42 +0100

    js fait

--- static/js/js_grille.js
+++ static/js/js_grille.js
@@ -1,6 +1,6 @@
         window.onload = () => genere_grille(id_game);
         tour_joueur();
-        //fonction de génération de l'image de grille : 
+        // Fonction de génération de l'image de grille
         async function genere_grille(id_game) {
 
             const response = await fetch("/generate", {
@@ -15,7 +15,7 @@
             const result = await response.json();
 
             if (response.ok) {
-                // Met a jour l'image
+                // Met à jour l'image
                 document.getElementById("output-image").src = result.image_url + '?' + new Date().getTime();
             } else {
                 alert(result.error || "Une erreur a eu lieu");
@@ -32,7 +32,7 @@
             });
 
             const result = await response.json();
-
+            // Met à jour la couleur du joueur à qui c'est le tour sur la page
             if (response.ok) {
                 document.getElementById("couleur_joueur").textContent = result.joueur;
                 const couleurElement = document.getElementById('couleur_joueur');
@@ -52,18 +52,19 @@
                     couleurElement.style.color = 'white';
                     couleurElement.style.backgroundColor = 'green';
                 }
-                // Met a jour l'image
+                // Met à jour
                 document.getElementById("couleur_joueur").textContent = result.joueur;
             } else {
                 alert(result.error || "Une erreur a eu lieu pour le joueur");
             }
         }
+
         function updategrille(){
             genere_grille(id_game)
         }
         
+        // Redirige pour finir la partie
         function fin_de_partie() {
-            console.log("partie finie");
             window.location.href = `/fin_de_partie/${id_game}`;
         }
         
@@ -73,6 +74,7 @@
             glisseElement(element);
         });
 
+        // Pour avoir des variables globales
         var elementReel = ""
         var retourne = 1
         var rotation = 0
@@ -80,8 +82,8 @@
         var carreY = 0
         let envoie = null
 
+        // Renvoie les informations de la pièce vers le serveur
         async function envoieValeur(carrX, carrY, retourne, rotation , element) {
-            console.log("retourne et rotation :", retourne, rotation)
             const response = await fetch("/submit22", { 
                 method: "POST",
                 credentials: 'include',
@@ -92,35 +94,33 @@
             });
         
             if (response.ok) {
-                console.log("reponseok")
                 const result = await response.json();
+                // Si le coup est considéré comme valide
                 if (result.status == "coup valide"){
-                    console.log("coup valide");
                     genere_grille(id_game);
                     var nonid = document.getElementById(element);
-                    console.log("nb_joueur",nb_joueur)
                     nonid.remove();
                     if (nb_joueur == 1 || nb_joueur == 2){
-                        console.log("changement de joueur")
                         location.reload()
                     }
-
-                console.log('Réponse du serveur:', result);}
+                }
+                // Si le joueur joue alors que ce n'est pas son tour
                 else if (result.status == "pas le bon tour"){
                     setTimeout(function() {
-                        document.getElementById('tempo').classList.add('visible'); // Ajouter la classe 'visible' pour rendre l'élément visible avec animation
+                        // Rend l'élément visible
+                        document.getElementById('tempo').classList.add('visible');
                     }, 10); 
                     setTimeout(function() {
-                        document.getElementById('tempo').classList.remove('visible'); // Enlève la classe 'visible' pour rendre l'élément visible avec animation
+                        // Rend l'élément invisible avec animation
+                        document.getElementById('tempo').classList.remove('visible');
                     }, 3000); 
                 }
                 else if (result.status == "coup interdit"){
-                    // Après avoir ajouté l'élément, ajouter la classe 'visible' après un court délai pour déclencher l'animation
                     setTimeout(function() {
-                        document.getElementById('temporaire').classList.add('visible'); // Ajouter la classe 'visible' pour rendre l'élément visible avec animation
+                        document.getElementById('temporaire').classList.add('visible');
                     }, 10); 
                     setTimeout(function() {
-                        document.getElementById('temporaire').classList.remove('visible'); // Enlève la classe 'visible' pour rendre l'élément visible avec animation
+                        document.getElementById('temporaire').classList.remove('visible');
                     }, 3000); 
                 }
                 }
@@ -134,12 +134,13 @@
             let doubleClic = false;
             element.dataset.flip = "1";
             element.dataset.rotation = "0";
-            // Quand la souris sélectionne l'image, on lance une nouvelle fonction
+            // Quand la souris double-clic sur l'image, on lance une nouvelle fonction
             element.addEventListener('dblclick',function (event){
                 document.addEventListener('contextmenu', function(event) {
                     event.preventDefault();
                 })
                 flip(element.querySelector("img"))
+                // Pour ne pas avoir en plus l'effet du clic gauche/droit
                 doubleClic = true;
             })
             element.querySelector("img").onmousedown = function(event){
@@ -151,6 +152,7 @@
                     return;
                 }
                 else if (event.button === 2) {
+                    // Action de clic gauche
                     tourne(element.querySelector("img"));
                     return;
                 }
@@ -160,10 +162,12 @@
                 }
             }
         }
-        const flipSound = new Audio('/static/sounds/flip.wav');  
+        const flipSound = new Audio('/static/sounds/flip.wav');
+        // Pour retourner l'image
             function flip(e) {
                 flipSound.play();
                 var scaleX_valeur = window.getComputedStyle(e).transform;
+                // Si non flip
                 if (scaleX_valeur === 'none' || scaleX_valeur === 'matrix(1, 0, 0, 1, 0, 0)' || scaleX_valeur === 'matrix(0, -1, 1, 0, 0, 0)' || scaleX_valeur === 'matrix(0, 1, -1, 0, 0, 0)' || scaleX_valeur === 'matrix(-1, 0, 0, -1, 0, 0)') {
                     e.dataset.flip = "-1";
                     e.style.transform = `scaleX(-1)`;
@@ -180,8 +184,8 @@
                 const flip = transform.includes('scaleX(-1)');
                 // Donne valeur numérique de la rotation
                 rotation = e.style.transform.replace(/[^\d.]/g, '') || 0;
+                // Si flip, valeur "aléatoire"
                 if (flip) {
-                    console.log(rotation)
                     if (rotation == 1){
                         rotation = 0
                     }
@@ -194,21 +198,17 @@
                     if (rotation > 181){
                         rotation = rotation - 900
                     }
-                    console.log(rotation)
                     // Rotation de 90 degrés à partir de l'image actuelle
                     rotation = (parseInt(rotation) + 90) % 360;
                     e.dataset.rotation = `${rotation}`;
-                    console.log(rotation)
                     e.style.transform = `scaleX(-1) rotate(${rotation}deg)`;
                 }
                 else {
-                    console.log(rotation)
                     if (rotation == 1){
                         rotation = 0
                     }
                     // Rotation de 90 degrés à partir de l'image actuelle
                     rotation = (parseInt(rotation) + 90) % 360;
-                    console.log(rotation)
                     e.dataset.rotation = `${rotation}`;
                     e.style.transform = `rotate(-${rotation}deg)`;
                 }
@@ -217,10 +217,10 @@
             function glisseSourisAppuie(e,element) {
                 // Empêche le comportement par défaut du navigateur
                 e.preventDefault();
-                // Capturer la position de la souris au moment du clic
+                // Capture la position de la souris au moment du clic
                 pos3 = e.clientX;
                 pos4 = e.clientY;
-                // Attacher les événements pour déplacer l'élément
+                // Attache les événements pour déplacer l'élément
                 document.onmouseup = function() { arreteGlisseElement(element); };
                 document.onmousemove = function(event) { elementGlisse(event, element); };
             }
@@ -232,7 +232,7 @@
                 pos2 = pos4 - e.clientY;
                 pos3 = e.clientX;
                 pos4 = e.clientY;
-                // Déplacer l'élément, element.offset... donne la position entre le parent et l'élément
+                // Déplace l'élément, element.offset... donne la position entre le parent et l'élément
                 element.style.top = (element.offsetTop - pos2) + "px";
                 element.style.left = (element.offsetLeft - pos1) + "px";
             }
@@ -257,15 +257,14 @@
             
             function placementGrille(x,y,element) {
                 var element = element.querySelector("img")
-                // Le numéro de la case
+                // Donne le numéro de la case
                 carreX = Math.round(((x-120)/760)*19);
                 carreY = Math.round(((y-120)/760)*19);
                 // Affiche le carré où va la pièce
                 var carreText = "X: " + carreX + ", Y: " + carreY;
                 document.getElementById("carre").textContent = carreText;
                 elementReel = element.id;
-                // Pour ne pas appeler valider le nombre de fois où l'on a cliqué sur le bouton Valider 
-                console.log(envoie)
+                // Pour ne pas appeler valider() le nombre de fois où l'on a cliqué sur le bouton Valider
                 if (envoie != null) {
                     document.getElementById("myButton").removeEventListener("click",envoie);
                 }
@@ -275,6 +274,7 @@
                 document.getElementById("myButton").addEventListener("click",envoie);
             }
 
+            // Fonction du bouton Valider
             function valider(element) {
                 const imgElement = document.getElementById(element);
                 const retourne = parseInt(imgElement.dataset.flip) ||1;
